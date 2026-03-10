7d27ea768281fc8c9522976119bc4515e287df7b
victorsismotto2292 <vgr.sismotto@gmail.com>
2026-03-10 18:14:50 -0300

    Pasta exemplo

---
+++ exemplo_radio_label/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'exemplo_radio_label',
+  webDir: 'www'
+};
+
+export default config;
