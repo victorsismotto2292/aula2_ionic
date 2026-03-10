fa441f406e556368350eafda1155ee8dd71c3d4c
victorsismotto2292 <vgr.sismotto@gmail.com>
2026-03-10 19:42:41 -0300

    Exercício 1 concluído

--- exercicios/src/app/home/home.page.ts
+++ exercicios/src/app/home/home.page.ts
@@ -9,5 +9,26 @@ import { Component } from '@angular/core';
 export class HomePage {
 
   constructor() {}
+  valor_compra = '';
+  res_ex1 = '';
+  tipoDesconto = '';
+  alertButtons = ['OK'];
 
+  Desconto(){
+    const valor = parseFloat(this.valor_compra);
+    if(isNaN(valor)){
+      this.res_ex1 = 'Por favor, digite algum valor.'
+    }
+    else if(this.tipoDesconto == 'vista'){
+      const desconto_vista = valor - (valor*0.05);
+      this.res_ex1 = `${desconto_vista}`;
+    }
+    else if(this.tipoDesconto == 'prazo'){
+      this.res_ex1 = `${valor}`;
+    }
+    else if(this.tipoDesconto == 'PIX'){
+      const desconto_PIX = valor - (valor*0.15);
+      this.res_ex1 = `${desconto_PIX}`;
+    }
+  }
 }
